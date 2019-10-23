2afc6e42d5431f28980cfbdc6feb9788bb5a7605
Chris Perko <chris@chrisperko.net>
2019-10-23 19:05:50 +0000

    Export complete #25

--- templates/exportImport.php
+++ templates/exportImport.php
@@ -1,16 +1,16 @@
 <div class="wrap">
     <h1>WPCUI Export/Import</h1>
 
+    <p>Copy the text below to another WordPress installation.</p>
+
 	<?php use PerkoCustomizerUI\Services\DataService; ?>
 
     <?php 
     $sections = DataService::getSections(); 
-    var_dump($sections);
-
-    echo '<br>';
-
     $controls = DataService::getControls();
-    var_dump($controls);
+    $export = ["sections" => $sections, "controls" => $controls];
     ?>
+
+    <textarea readonly rows="10" cols="100"><?= json_encode($export); ?></textarea>
 
 </div>
