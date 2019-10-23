39b95731f4e6494e73916e3373a5acb9d64d3473
Chris Perko <chris@chrisperko.net>
2019-10-23 18:58:00 +0000

    Display db data for export #25

--- templates/exportImport.php
+++ templates/exportImport.php
@@ -1,6 +1,16 @@
 <div class="wrap">
     <h1>WPCUI Export/Import</h1>
 
-	
+	<?php use PerkoCustomizerUI\Services\DataService; ?>
+
+    <?php 
+    $sections = DataService::getSections(); 
+    var_dump($sections);
+
+    echo '<br>';
+
+    $controls = DataService::getControls();
+    var_dump($controls);
+    ?>
 
 </div>
